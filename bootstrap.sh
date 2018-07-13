799368af5e65349b58de57cf88cdfe440d62843a
David Cordero <david@corderoramirez.com>
2018-07-13 21:13:50 +0200

    Organize bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,83 +1,89 @@
-#!/bin/sh
 
-echo "Configure powerline-shell"
-ln -s `pwd`/powerline-shell.py ~/.powerline-shell.py
+#!/usr/bin/env sh
+RED="\033[31m"
+GREEN="\033[32m"
+YELLOW="\033[33m"
+RESTORE='\033[0m'
 
-echo "Configure Bash"
-ln -s `pwd`/bash_profile ~/.bash_profile
+installApps() {
+    echo "${YELLOW}Install Homebrew${RESTORE}"
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+    echo "${YELLOW}Install other tools${RESTORE}"
+    brew install mc
+    brew install imagemagick
+    brew install hub
+    brew install bundler
+    brew cast install iterm2
+    brew install m-cli
+    brew install python
+    brew install vim --with-override-system-vi --with-ruby
 
-echo "Link iCloud from home folder"
-ln -s ~/Library/Mobile\ Documents/com~apple~CloudDocs ~/iCloud
+    echo "${YELLOW}Install and configure Git${RESTORE}"
+    brew install git
+}
 
-echo "Install Homebrew"
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+configure() {
+    echo "${YELLOW}Configure powerline-shell${RESTORE}"
+    ln -s `pwd`/powerline-shell.py ~/.powerline-shell.py
 
-echo "Install CTags"
-brew install ctags
-alias ctags="`brew --prefix`/bin/ctags"
+    echo "${YELLOW}Configure Bash${RESTORE}"
+    ln -s `pwd`/bash_profile ~/.bash_profile
 
-echo "Install and configure midnight commander"
-brew install mc
+    echo "${YELLOW}Link iCloud from home folder${RESTORE}"
+    ln -s ~/Library/Mobile\ Documents/com~apple~CloudDocs ~/iCloud
 
-echo "Install image magic"
-brew install imagemagick
+    echo "${YELLOW}Configure Git${RESTORE}"
+    ln -s `pwd`/gitconfig ~/.gitconfig
 
-echo "Install and configure Git"
-brew install git
-ln -s `pwd`/gitconfig ~/.gitconfig
+    echo "${YELLOW}Configure Vim${RESTORE}"
+    ln -s `pwd`/vimrc ~/.vimrc
+    mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+    curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+    cd ~/.vim/bundle
+    git clone https://github.com/scrooloose/nerdtree.git
+    git clone https://github.com/ivalkeen/nerdtree-execute
+    git clone https://github.com/Xuyuanp/nerdtree-git-plugin.git
+    git clone git://github.com/dhruvasagar/vim-vinegar.git
+    git clone https://github.com/keith/swift.vim.git
+    git clone https://github.com/bronson/vim-trailing-whitespace.git
+    cd command-t && rake make && cd ..
+    cd -
 
-echo "Install and configure Vim"
-export C_INCLUDE_PATH=/System/Library/Frameworks/Python.framework/Headers:$C_INCLUDE_PATH
-export CPLUS_INCLUDE_PATH=/System/Library/Frameworks/Python.framework/Headers:$CPLUS_INCLUDE_PATH
-brew install python
-brew install vim --with-override-system-vi --with-ruby
-ln -s `pwd`/vimrc ~/.vimrc
+    echo "${YELLOW}Setup walpaper${RESTORE}"
+    m wallpaper ./wallpaper.jpg
 
-mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+    echo "${YELLOW}Avoid iTunes launch when pressing media buttons${RESTORE}"
+    launchctl unload -w /System/Library/LaunchAgents/com.apple.rcd.plist
 
-cd ~/.vim/bundle
-git clone https://github.com/scrooloose/nerdtree.git
-git clone https://github.com/ivalkeen/nerdtree-execute
-git clone https://github.com/Xuyuanp/nerdtree-git-plugin.git
-git clone git://github.com/dhruvasagar/vim-vinegar.git
-git clone https://github.com/keith/swift.vim.git
-git clone https://github.com/bronson/vim-trailing-whitespace.git
-cd command-t && rake make && cd ..
-cd -
+    echo "${YELLOW}Activate Debug mode in Safari${RESTORE}"
+    defaults write com.apple.Safari IncludeDebugMenu 1
 
-echo "Install m-cli"
-brew install m-cli
+    echo "${YELLOW}Configure Visual Studio Code${RESTORE}"
+    ln -s `pwd`/vscode.json Library/Application\ Support/Code/User/settings.json
 
-echo "Set wallpaper"
-m wallpaper ./wallpaper.jpg
+    echo "${YELLOW}Disable Press and Hold in VSCode${RESTORE}"
+    defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
 
-echo "Avoid iTunes launch when pressing media buttons"
-launchctl unload -w /System/Library/LaunchAgents/com.apple.rcd.plist
+    echo "${YELLOW}Configure Dock${RESTORE}"
+    m dock autohide YES
+    m dock magnification YES
+    m dock position BOTTOM
 
-echo "Activate Debug mode in Safari"
-defaults write com.apple.Safari IncludeDebugMenu 1
+    echo "${YELLOW}Configure Finder${RESTORE}"
+    m finder showhiddenfiles YES
+    m finder showextensions YES
 
-echo "Configure Dock"
-m dock autohide YES
-m dock magnification YES
-m dock position BOTTOM
+    echo "${YELLOW}Configure custom Xcode theme${RESTORE}"
+    mkdir -p ~/Library/Developer/Xcode/UserData/FontAndColorThemes/
+    ln -s `pwd`/dcordero.dvtcolortheme ~/Library/Developer/Xcode/UserData/FontAndColorThemes/dcordero.dvtcolortheme
 
-echo "Configure Finder"
-m finder showhiddenfiles YES
-m finder showextensions YES
+    echo "${YELLOW}Hide useless predefined folders under Home${RESTORE}"
+    chflags hidden ~/Music
+    chflags hidden ~/Movies
+    chflags hidden ~/Pictures
+    chflags hidden ~/Public
+}
 
-echo "Configure custom Xcode theme"
-mkdir -p ~/Library/Developer/Xcode/UserData/FontAndColorThemes/
-ln -s `pwd`/dcordero.dvtcolortheme ~/Library/Developer/Xcode/UserData/FontAndColorThemes/dcordero.dvtcolortheme
-
-echo "Hide useless predefined folders under Home"
-chflags hidden ~/Music
-chflags hidden ~/Movies
-chflags hidden ~/Pictures
-chflags hidden ~/Public
-
-echo "Configure Visual Studio Code"
-ln -s `pwd`/vscode.json Library/Application\ Support/Code/User/settings.json
-
+installApps()
+configure()
